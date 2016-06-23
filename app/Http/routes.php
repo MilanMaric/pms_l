9ca75a91077693e4d49267eb541df6165b4718af
RockLegend93 <milan.maric.ja@gmail.com>
2016-06-23 10:02:40 +0200

    Swagger published

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -31,3 +31,16 @@ Route::group(['prefix' => 'api', 'namespace' => 'API'], function () {
         require config('infyom.laravel_generator.path.api_routes');
     });
 });
+
+
+/*
+|--------------------------------------------------------------------------
+| API routes
+|--------------------------------------------------------------------------
+*/
+
+Route::group(['prefix' => 'api', 'namespace' => 'API'], function () {
+    Route::group(['prefix' => 'v1'], function () {
+        require config('infyom.laravel_generator.path.api_routes');
+    });
+});
