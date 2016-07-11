7a45b0f0bac98ad8237428346a8be73e5fe92c8d
RockLegend93 <milan.maric.ja@gmail.com>
2016-07-11 20:13:14 +0200

    Added Expense validation

--- app/Models/Expense.php
+++ app/Models/Expense.php
@@ -51,7 +51,7 @@ class Expense extends Model
     use SoftDeletes;
 
     public $table = 'expense';
-    
+
     const CREATED_AT = 'created_at';
     const UPDATED_AT = 'updated_at';
 
@@ -90,6 +90,7 @@ class Expense extends Model
      * @var array
      */
     public static $rules = [
-        
+        'Amount' => 'required|min:0',
+        'project_id' => 'required'
     ];
 }
